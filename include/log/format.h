b9be2f7237a6d71aa58de1afd39651b7c91b60a6
2mu <1730911462@qq.com>
2024-08-18 10:41:49 +0800

    :recycle: refactor: 日志格式解析函数重写, 让可读性更好

--- include/log/format.h
+++ include/log/format.h
@@ -8,6 +8,7 @@
 #include "log/level.h"
 #include "log/event.h"
 
+
 /**
  * @brief 日志格式化内容项(基类)
  */
@@ -29,11 +30,15 @@ public:
      * @param[in] level 日志等级
      * @param[in] event 日志事件
      */
-    virtual void format(std::ostream &os, std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event) = 0;
+    virtual void format(std::ostream &os, std::shared_ptr<Logger> logger, 
+        LogLevel::Level level, LogEvent::ptr event) = 0;
+
 
 protected:
-    std::string     m_format;/// 日志项的格式(目前只有日期时间项有格式)
+    // 日志项的格式(目前只有日期时间项有格式)
+    std::string     m_format;
 };
+
 
 /**
  * @brief 消息内容项
@@ -45,11 +50,13 @@ public:
         : LogFormatItem(format)
     {}
 
-    void format(std::ostream &os, std::shared_ptr<Logger> logger __attribute__((unused)), LogLevel::Level level __attribute__((unused)), LogEvent::ptr event) override
+    void format(std::ostream &os, std::shared_ptr<Logger> logger __attribute__((unused)), 
+        LogLevel::Level level __attribute__((unused)), LogEvent::ptr event) override
     {
         os << event->getContent();
     }
 };
+
 
 /**
  * @brief 日志等级项
@@ -61,11 +68,13 @@ public:
         : LogFormatItem(format)
     {}
 
-    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)), LogLevel::Level level, LogEvent::ptr event __attribute__((unused))) override 
+    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)),
+        LogLevel::Level level, LogEvent::ptr event __attribute__((unused))) override 
     {
         os << LogLevel::ToString(level);
     }
 };
+
 
 /**
  * @brief 线程id项
@@ -77,11 +86,13 @@ public:
         : LogFormatItem(format)
     {}
 
-    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)), LogLevel::Level level __attribute__((unused)), LogEvent::ptr event) override 
+    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)),
+        LogLevel::Level level __attribute__((unused)), LogEvent::ptr event) override 
     {
         os << event->getThreadID();
     }
 };
+
 
 /**
  * @brief 线程名
@@ -93,11 +104,13 @@ public:
         : LogFormatItem(format)
     {}
 
-    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)), LogLevel::Level level __attribute__((unused)), LogEvent::ptr event) override 
+    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)), 
+        LogLevel::Level level __attribute__((unused)), LogEvent::ptr event) override 
     {
         os << event->getThreadName();
     }
 };
+
 
 /**
  * @brief 换行符项
@@ -109,11 +122,13 @@ public:
         : LogFormatItem(format)
     {}
 
-    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)), LogLevel::Level level __attribute__((unused)), LogEvent::ptr event __attribute__((unused))) override 
+    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)), 
+        LogLevel::Level level __attribute__((unused)), LogEvent::ptr event __attribute__((unused))) override 
     {
         os << std::endl;
     }
 };
+
 
 /**
  * @brief 行号项
@@ -125,11 +140,13 @@ public:
         : LogFormatItem(format)
     {}
 
-    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)), LogLevel::Level level __attribute__((unused)), LogEvent::ptr event) override 
+    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)), 
+        LogLevel::Level level __attribute__((unused)), LogEvent::ptr event) override 
     {
         os << event->getLine();
     }
 };
+
 
 /**
  * @brief 文件名项
@@ -141,11 +158,13 @@ public:
         : LogFormatItem(format)
     {}
 
-    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)), LogLevel::Level level __attribute__((unused)), LogEvent::ptr event) override 
+    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)),
+        LogLevel::Level level __attribute__((unused)), LogEvent::ptr event) override 
     {
         os << event->getFile();
     }
 };
+
 
 /**
  * @brief Tab键项
@@ -157,11 +176,13 @@ public:
         : LogFormatItem(format)
     {}
 
-    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)), LogLevel::Level level __attribute__((unused)), LogEvent::ptr event __attribute__((unused))) override 
+    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)),
+        LogLevel::Level level __attribute__((unused)), LogEvent::ptr event __attribute__((unused))) override 
     {
         os << '\t';
     }
 };
+
 
 /**
  * @brief 时间日期项，内部调用strftime设定子格式
@@ -177,11 +198,13 @@ public:
             m_format = "%Y-%m-%d %H:%M:%S";
     }
 
-    void format(std::ostream& os, std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event) override;
+    void format(std::ostream& os, std::shared_ptr<Logger> logger, LogLevel::Level level,
+        LogEvent::ptr event) override;
 };
 
+
 /**
- * @brief 格式中不需要转义的项，原封不动，直接输出。
+ * @brief 格式中不需要转义的项, 直接原样输出
  */
 class StringFormatItem : public LogFormatItem 
 {
@@ -190,7 +213,8 @@ public:
         :m_string(str)
     {}
 
-    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)), LogLevel::Level level __attribute__((unused)), LogEvent::ptr event __attribute__((unused))) override 
+    void format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)),
+        LogLevel::Level level __attribute__((unused)), LogEvent::ptr event __attribute__((unused))) override 
     {
         os << m_string;
     }
@@ -198,6 +222,7 @@ public:
 private:
     std::string m_string;
 };
+
 
 /**
  * @brief 将LogEvent内容格式化，转化成string
@@ -208,13 +233,13 @@ public:
     typedef std::shared_ptr<LogFormatter> ptr;
     /**
      * @brief 构造函数
-     * @param[in] pattern 日志格式模板,可以设置的项如下
+     * @param[in] pattern 日志格式模板, 可以设置的项如下
      * @details 
      *  %m 消息内容
      *  %p 日志级别
      *  %t 线程id
      *  %n 换行
-     *  %d 日期时间,后面加{}指定子格式，strftime的格式
+     *  %d 日期时间, 后面加{}指定子格式, 也就是strftime的格式
      *  %f 文件名
      *  %l 行号
      *  %T 制表符
@@ -222,11 +247,13 @@ public:
      */
     LogFormatter(const std::string& pattern);
 
-    std::string format(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event);
-    std::ostream& format(std::ostream& ofs, std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event);
+    std::string format(std::shared_ptr<Logger> logger, LogLevel::Level level,
+            LogEvent::ptr event);
+    std::ostream& format(std::ostream& ofs, std::shared_ptr<Logger> logger, 
+            LogLevel::Level level, LogEvent::ptr event);
 
     /**
-     * @brief 初始化，解析日志模板（重点）
+     * @brief 初始化, 即解析日志格式字符串m_pattern
      */
     void init();
 
@@ -248,10 +275,17 @@ public:
 
 
 private:
+    /**
+     * @brief 从m_pattern指定位置开始, 获取{}子格式项
+     * @param pos 开始搜索子格式位置
+     * @return sub_format 搜索到的子格式, 若没有, 则返回空字符串;
+     */
+    std::string get_item_format(size_t pos) const;
+
     // 日志格式模板
     std::string                     m_pattern;
     std::vector<LogFormatItem::ptr> m_items;
-    bool                            m_error = false;
+    bool                            m_error;
 };
 
 #endif // LOG_FORMAT_H
--- src/log/format.cpp
+++ src/log/format.cpp
@@ -1,13 +1,13 @@
 #include "log/format.h"
 
-#include <iostream>
-#include <sstream>
 #include <map>
 #include <functional>
 #include <cctype>
 #include <time.h>
 
-void DateTimeFormatItem::format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)), LogLevel::Level level __attribute__((unused)), LogEvent::ptr event)
+
+void DateTimeFormatItem::format(std::ostream& os, std::shared_ptr<Logger> logger __attribute__((unused)),
+    LogLevel::Level level __attribute__((unused)), LogEvent::ptr event)
 {
     time_t time = event->getTime();
     struct tm t;
@@ -17,8 +17,9 @@ void DateTimeFormatItem::format(std::ostream& os, std::shared_ptr<Logger> logger
     os << buf;
 }
 
+
 LogFormatter::LogFormatter(const std::string& pattern)
-    :m_pattern(pattern)
+    : m_pattern(pattern), m_error(false)
 {
     init();
 }
@@ -42,125 +43,113 @@ std::ostream& LogFormatter::format(std::ostream& ofs, std::shared_ptr<Logger> lo
     return ofs;
 }
 
+std::string LogFormatter::get_item_format(size_t pos) const
+{
+    std::string format;
+    if (pos > m_pattern.size())
+        return format;
+    if (m_pattern[pos] == '{')
+    {
+        size_t end_pos = pos + 1;
+        while(end_pos < m_pattern.size() && m_pattern[end_pos] != '}')
+        {
+            ++end_pos;
+        }
+        if(end_pos < m_pattern.size() && m_pattern[end_pos] == '}')
+            format = m_pattern.substr(pos + 1, end_pos - pos - 1);
+    }
+    // 要么返回空串, 要么返回不带{}的项格式
+    return format;
+}
+
 void LogFormatter::init()
 {
-    // 字符串映射到创建对应FormatterItem对象的lambda
-    static std::map<std::string, std::function<LogFormatItem::ptr(const std::string& str)> > s_format_items = {
-#define XX(str, C) \
-        {#str, [](const std::string& fmt) { return LogFormatItem::ptr(new C(fmt));}}
-
-            XX(m, MessageFormatItem),           //m:消息
-            XX(p, LevelFormatItem),             //p:日志级别
-            XX(t, ThreadIdFormatItem),          //t:线程id
-            XX(n, NewLineFormatItem),           //n:换行
-            XX(d, DateTimeFormatItem),          //d:日期时间
-            XX(f, FilenameFormatItem),          //f:文件名
-            XX(l, LineFormatItem),              //l:行号
-            XX(T, TabFormatItem),               //T:Tab
-            XX(N, ThreadNameFormatItem),        //N:线程名称
-#undef XX
+    // 字符串映射到创建对应FormatterItem对象的lambda; 如果是全局变量, 全局变量还需要考虑初始化顺序...
+    static std::map<char, std::function<LogFormatItem::ptr(const std::string& fmt)> > g_format_items 
+        = {
+        #define init_map(ch, cls) \
+            {ch, [](const std::string& fmt) { return LogFormatItem::ptr(new cls(fmt));}}
+                init_map('m', MessageFormatItem),           //m:消息
+                init_map('p', LevelFormatItem),             //p:日志级别
+                init_map('t', ThreadIdFormatItem),          //t:线程id
+                init_map('n', NewLineFormatItem),           //n:换行
+                init_map('d', DateTimeFormatItem),          //d:日期时间
+                init_map('f', FilenameFormatItem),          //f:文件名
+                init_map('l', LineFormatItem),              //l:行号
+                init_map('T', TabFormatItem),               //T:Tab
+                init_map('N', ThreadNameFormatItem),        //N:线程名称
+        #undef init_map
     };
 
-
-    // @todo 找机会优化下, 以下这些逻辑不太好阅读...
-
-
-    // 元组(项, 子格式, 是否需要转义)数组
-    std::vector<std::tuple<std::string, std::string, int>> pattern_info;
-    std::string sub_str;// pattern中的某项
+    std::string item_string;
     for(size_t i = 0; i < m_pattern.size(); ++i)
     {
-        if(m_pattern[i] != '%')
+        const char ch = m_pattern[i];
+        if (ch == '%')
         {
-            sub_str.append(1, m_pattern[i]);
-            continue;
-        }
-
-        // 如果最后一个字符是%，那就不是转义
-        if((i + 1) < m_pattern.size()) 
-        {
-            if(m_pattern[i + 1] == '%') 
+            if(!item_string.empty())
             {
-                sub_str.append(1, '%');
-                continue;
+                m_items.push_back(std::make_shared<StringFormatItem>(item_string));
+                item_string.clear();
             }
-        }
 
-        int fmt_status = 0;
-        size_t fmt_begin = 0;
+            if (i + 1 < m_pattern.size())
+            {
+                const char next_ch = m_pattern[i + 1];
+                if(std::isalpha(next_ch))
+                {
+                    // %后面接一个字母, 表示是合法格式; 此时继续判断该项是否有 子格式
+                    std::string item_format = this->get_item_format(i + 2);
+                    if(!item_format.empty())
+                        i = i + item_format.length() + 2;// i跳过项的format
 
-        std::string str;
-        std::string fmt;
-        size_t n = i + 1;
-        // 解读{}子格式，主要%d（时间）可以指定子格式
-        while(n < m_pattern.size()) {
-            if(!fmt_status && (!isalpha(m_pattern[n]) && m_pattern[n] != '{'
-                    && m_pattern[n] != '}')) {
-                str = m_pattern.substr(i + 1, n - i - 1);
-                break;
-            }
-            if(fmt_status == 0) {
-                if(m_pattern[n] == '{') {
-                    str = m_pattern.substr(i + 1, n - i - 1);
-                    fmt_status = 1; // 需要解析格式
-                    fmt_begin = n;
-                    ++n;
-                    continue;
+                    auto iter = g_format_items.find(next_ch);
+                    if(iter != g_format_items.end())
+                    {
+                        m_items.push_back(iter->second(item_format));
+                    }
+                    else
+                    {
+                        // 错误格式
+                        std::string error_info = " ->unknown format: %";
+                        error_info.push_back(next_ch);
+                        error_info += item_format;
+                        error_info += "<-";
+                        m_items.push_back(std::make_shared<StringFormatItem>(error_info));
+                        m_error = true;
+                    }
                 }
-            } else if(fmt_status == 1) {
-                if(m_pattern[n] == '}') {
-                    fmt = m_pattern.substr(fmt_begin + 1, n - fmt_begin - 1);
-                    fmt_status = 0; // 不需要解析格式，原样输出就行
-                    ++n;
-                    break;
+                else if(next_ch == '%')
+                {
+                    // 两个连续的 %% 转义为 %
+                    item_string.push_back('%');
                 }
-            }
-            ++n;
-            if(n == m_pattern.size()) {
-                if(str.empty()) {
-                    str = m_pattern.substr(i + 1);
+                else
+                {
+                    // 错误格式, %后字母不支持
+                    std::string error_info = " ->error format: %";
+                    error_info.push_back(next_ch);
+                    error_info += "<-";
+                    m_items.push_back(std::make_shared<StringFormatItem>(error_info));
+                    m_error = true;
                 }
-            }
-        }
 
-        if(fmt_status == 0) 
-        {
-            if(!sub_str.empty()) {
-                pattern_info.push_back(std::make_tuple(sub_str, std::string(), 0));
-                sub_str.clear();
+                // %后面的字母已读取过, 跳过下次循环;
+                ++i;
             }
-            pattern_info.push_back(std::make_tuple(str, fmt, 1));
-            i = n - 1;
-        } 
-        else if(fmt_status == 1) 
-        {
-            std::cout << "pattern parse error: " << m_pattern << " - " << m_pattern.substr(i) << std::endl;
-            m_error = true;
-            pattern_info.push_back(std::make_tuple("<<pattern_error>>", fmt, 0));
-        }
-    }
-    if(!sub_str.empty()) {
-        pattern_info.push_back(std::make_tuple(sub_str, "", 0));
-    }
-
-    for(auto& tup : pattern_info)
-    {
-        if(std::get<2>(tup) == 0)
-        {
-            // 如果是不需要转义的, 原样输出string
-            m_items.push_back(LogFormatItem::ptr(new StringFormatItem(std::get<0>(tup))));
+            else
+            {
+                // 错误format, %后面未接具体项
+                std::string error_info = " ->error format: % <-";
+                m_items.push_back(std::make_shared<StringFormatItem>(error_info));
+                m_error = true;
+            }
         }
         else
         {
-            auto it = s_format_items.find(std::get<0>(tup));
-            if(it == s_format_items.end())
-            {
-                std::string error_info = "<< unknown format: %" + std::get<0>(tup) + ">>";
-                m_items.push_back(LogFormatItem::ptr(new StringFormatItem(error_info )));
-                m_error = true;
-            } 
-            else
-                m_items.push_back(it->second(std::get<1>(tup)));
+            item_string.push_back(ch);
         }
     }
+    if (!item_string.empty())
+        m_items.push_back(std::make_shared<StringFormatItem>(item_string));
 }
